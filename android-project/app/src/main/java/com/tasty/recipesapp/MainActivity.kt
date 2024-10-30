2bf407ab3a0bc520e62ff185a54c45a4c21647ec
LorinczNorbertAttila <norbertattila11@gmail.com>
2024-10-30 16:45:05 +0200

    Lab 4 - Recipe app splash screen

--- android-project/app/src/main/java/com/tasty/recipesapp/MainActivity.kt
+++ android-project/app/src/main/java/com/tasty/recipesapp/MainActivity.kt
@@ -12,10 +12,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        receiverMsg = findViewById(R.id.received_value_id)
-        val intent = intent
-        val str = intent.getStringExtra("message_key")
-        receiverMsg.text = str
+//        receiverMsg = findViewById(R.id.received_value_id)
+//        val intent = intent
+//        val str = intent.getStringExtra("message_key")
+//        receiverMsg.text = str
 
         Log.d(TAG, "onCreate: MainActivity created.")
     }
--- android-project/app/src/main/java/com/tasty/recipesapp/SplashActivity.kt
+++ android-project/app/src/main/java/com/tasty/recipesapp/SplashActivity.kt
@@ -4,8 +4,9 @@ import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
 import android.util.Log
-import android.widget.Button
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -21,13 +22,25 @@ class SplashActivity : AppCompatActivity() {
 
         Log.d(TAG, "onCreate: SplashActivity created.")
 
-        sendText = findViewById(R.id.send_text_id)
-        val button: Button = findViewById(R.id.button)
-        button.setOnClickListener {
-            val str = sendText.text.toString()
-            val intent = Intent(this@SplashActivity, MainActivity::class.java)
-            intent.putExtra("message_key", str)
-            startActivity(intent)
-        }
+//        sendText = findViewById(R.id.send_text_id)
+//        val button: Button = findViewById(R.id.button)
+//        button.setOnClickListener {
+//            val str = sendText.text.toString()
+//            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+//            intent.putExtra("message_key", str)
+//            startActivity(intent)
+//        }
+        // Use a HandlerThread to create a background thread
+        val handlerThread = HandlerThread("SplashHandlerThread", -10)
+        handlerThread.start() // Create a Handler on the new HandlerThread
+        val handler = Handler(handlerThread.looper)
+        val splashTimeout  = 2500
+        handler.postDelayed({
+        // Navigate to MainActivity after the delay
+        val intent = Intent(this@SplashActivity, MainActivity::class.java)
+        startActivity(intent)
+        finish() },
+            splashTimeout.toLong()
+        )
     }
 }
